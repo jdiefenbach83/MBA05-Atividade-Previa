0c74785c7aac0a3b0d332be279d9eb5508de558d
Jefferson Diefenbach <jefferson.diefenbach@gmail.com>
2020-12-26 15:44:09 -0300

    Deploing script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:jdiefenbach83/MBA05-Atividade-Previa.git master:gh-pages
+
+cd -
+
+#https://cli.vuejs.org/guide/deployment.html#github-pages
